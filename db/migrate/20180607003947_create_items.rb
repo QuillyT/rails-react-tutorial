97581a305f65ef1810b8b56ec56ecd1c41d8a9c6
Quentin Tai <quillyt@gmail.com>
2018-06-07 02:50:56 +0200

    Fix item model and migration.

--- db/migrate/20180607003947_create_items.rb
+++ db/migrate/20180607003947_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :description
-      t.integer :count
+      t.integer :name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2018_06_07_003947) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
-    t.integer "count"
+    t.integer "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
